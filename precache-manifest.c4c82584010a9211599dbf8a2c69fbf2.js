79cca80dc4a4030b68b426462e9546ff38c17959
swoboda <michal.swoboda@outlook.com>
2020-01-04 15:15:07 -0500

    Updates

---
+++ precache-manifest.c4c82584010a9211599dbf8a2c69fbf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56d2e8b0d3250108f8fdfba557204fd",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "53b63e90628ee38f180e",
+    "url": "/memory-game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d8d174628937d221b2f9",
+    "url": "/memory-game/static/css/main.8813a36c.chunk.css"
+  },
+  {
+    "revision": "53b63e90628ee38f180e",
+    "url": "/memory-game/static/js/2.d9d12a09.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/memory-game/static/js/2.d9d12a09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8d174628937d221b2f9",
+    "url": "/memory-game/static/js/main.5432b5f1.chunk.js"
+  },
+  {
+    "revision": "ffb99498c6aca822fff7",
+    "url": "/memory-game/static/js/runtime-main.d19c5671.js"
+  }
+]);
